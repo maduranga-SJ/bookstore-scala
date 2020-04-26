87ae44e2e2466a106f9fe39d0321a5182622707b
slgeeksfiverr <madurangasac@gmail.com>
2020-04-26 11:51:53 +0530

    In memory database created

---
+++ src/main/scala/Library.scala
@@ -0,0 +1,25 @@
+import scala.collection.mutable
+
+object Library {
+
+  val bookList: mutable.HashMap[String,Book] = mutable.HashMap.empty
+
+  def addBook(isbn: String, book: Book): String ={
+    bookList.put(isbn, book)
+    "Successfully Added the Book: " + book.toString
+  }
+  def getBook(isbn: String): Option[Book] = bookList.get(isbn)
+  def getAllBooks: List[Book] = bookList.values.toList
+  def searchBook(query_param: String): List[Book] = {
+
+    val searchListTitle = bookList.values.filter(_.title.contains(query_param)).toList
+    val searchListAuthor = bookList.values.filter(_.author.contains(query_param)).toList
+
+    if (searchListTitle.nonEmpty && searchListAuthor.nonEmpty){
+      return searchListTitle ++ searchListAuthor
+    } else if (searchListTitle.isEmpty && searchListAuthor.nonEmpty) {
+      return searchListAuthor
+    }
+    searchListTitle
+  }
+}
