7f7dc6cd8216344a6523cef92d4c44253c139382
Maduranga Jayasooriya <madurangasac@gmail.com>
2020-04-29 23:27:00 +0530

    Seperate Handlers

--- src/main/scala/controller/GetRequestHandler.scala
+++ src/main/scala/controller/GetRequestHandler.scala
@@ -1,5 +1,27 @@
 package controller
 
+import com.sun.net.httpserver.HttpExchange
+import dao.Library
+import scala.util.matching.Regex
+
 object GetRequestHandler {
 
+  private val GET_BOOK_SUCCESS_MESSAGE = "Successfully Retrieved the Book"
+  private val SEARCH_BOOK_SUCCESS_MESSAGE = "Successfully Retrieved the Matching Books"
+  private val GET_BOOK_LIST_SUCCESS_MESSAGE = "Successfully Retrieved All Books"
+  private val INVALID_URL_MESSAGE = "Invalid URL"
+
+  private val ISBNPattern: Regex = "(/books/book/)(.*)".r
+  private val TitleAuthorPattern: Regex = "(/books[?]q=)(.*)".r
+  private val ListPattern: Regex = "(/books/)".r
+
+  def apply(exchange: HttpExchange): Unit = {
+    val requestURI = exchange.getRequestURI.toString
+    requestURI match {
+      case ISBNPattern(_, param) => ResponseHandler(exchange, GetBookResponse(GET_BOOK_SUCCESS_MESSAGE, Library.getBook(param.trim.toLowerCase).orNull))
+      case TitleAuthorPattern(_,param) => ResponseHandler(exchange, GetBookListResponse(SEARCH_BOOK_SUCCESS_MESSAGE, Library.searchBook(param.trim.toLowerCase)))
+      case ListPattern(_) => ResponseHandler(exchange, GetBookListResponse(GET_BOOK_LIST_SUCCESS_MESSAGE, Library.getAllBooks))
+      case _ => ResponseHandler(exchange, InvalidRequestMethodResponse(INVALID_URL_MESSAGE))
+    }
+  }
 }
--- src/main/scala/controller/PostRequestHandler.scala
+++ src/main/scala/controller/PostRequestHandler.scala
@@ -1,5 +1,37 @@
 package controller
 
+import com.sun.net.httpserver.HttpExchange
+import io.circe.generic.semiauto.deriveDecoder
+import io.circe.{Decoder, parser}
+import model._
+import dao._
+
+import scala.io.Source
+import scala.util.matching.Regex
+
 object PostRequestHandler {
 
+  private val ADD_BOOK_SUCCESS_MESSAGE = "Successfully Added The Book"
+  private val INVALID_URL_MESSAGE = "Invalid Url to Add the Book"
+  private val INVALID_JSON_MESSAGE = """Invalid Json, Please Provide a JSON with {"isbn":<isbn>, "title":<title>, "author":<author>} """
+
+  private val addBookPattern: Regex = "(/books/book.*)".r
+
+  def apply(exchange: HttpExchange): Unit = {
+    val requestURI = exchange.getRequestURI.toString
+    requestURI match {
+      case addBookPattern(_) => addBook(exchange)
+      case _ => ResponseHandler(exchange, InvalidRequestMethodResponse(INVALID_URL_MESSAGE))
+    }
+  }
+
+  def addBook(exchange: HttpExchange): Unit ={
+    val jsonStr = Source.fromInputStream(exchange.getRequestBody).mkString
+    implicit val bookDecoder: Decoder[Book] = deriveDecoder[Book]
+    val decodeResult = parser.decode[Book](jsonStr)
+    decodeResult match {
+      case Right(book) => ResponseHandler(exchange, AddBookResponse( ADD_BOOK_SUCCESS_MESSAGE, Library.addBook(book.isbn, book).orNull))
+      case Left(error) => ResponseHandler(exchange, InvalidRequestMethodResponse(error.getMessage))
+    }
+  }
 }
